b66dde809d889cbbe4b6a3020b57f8266073391f
liomotolani <ligaliomotolani@gmail.com>
2018-10-31 21:36:46 +0000

    newphp

--- Signup.php
+++ Signup.php
@@ -2,19 +2,20 @@
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
-$password = $POST["password"];
+$password = $_POST["password"];
 $phonenumber = $_POST["phone_number"];
 $gender = $_POST["gender"];
 $country = $_POST["country"];
+saveToDatabase($firstname,$lastname,$email,$password,$phonenumber,$gender,$country);
 saveToFile($firstname,$lastname,$email,$password,$phonenumber,$gender,$country);
 header('Location:success.html');
 
 function saveToFile($firstname, $lastname, $email, $password, $phonenumber, $gender, $country)
 {
-$fileHandler = fopen("signuprecord.txt", "a");
-$string = $firstname .  ","  .$lastname . "," .$email. "," .$password. "," .$phonenumber. "," .$gender. "," .$country. "<br>";
-fwrite($fileHandler, $string);
-fclose($fileHandler);
+ $fileHandler = fopen("signuprecord.txt", "a");
+ $string = $firstname .  ","  . $lastname . "," . $email . "," . $password . "," . $phonenumber . "," . $gender . "," . $country . "<br>";
+ fwrite($fileHandler, $string);
+ fclose($fileHandler);
 }
 function saveToDatabase($firstname,$lastname,$email,$password,$phonenumber,$gender,$country)
 {
@@ -29,7 +30,7 @@ if (!$conn) {
     die("Connection failed: " .mysqli_connect_error());
 }
 $sql = "INSERT INTO user (firstname,lastname,email,password,phone_number,gender,country)
-VALUES ( '$firstname','$lastname','$email','$password','$phonenumber','$gender','$country')";
+VALUES ( '$firstname','$lastname','$email','$password','$phonenumber','$gender','$country',NOW())";
 if (!$result){
     die("Error: " . $sql . "<br>" . mysqli_error($conn));
 }
